e0af6efbd6c43b7eb45676ad8d149dd25435f289
Hlib Hryshko <hlib@siip.group>
2024-11-20 17:31:12 +0100

    deposit and withdraw functions are tested

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,10 +12,30 @@ public class Main {
         accounts.add(new FixedDepositAccount(20000, 7, 3)); // Principal: 20,000, Rate: 7%, Time: 3 years
         accounts.add(new RecurringDepositAccount(5000, 6, 1)); // Principal: 5,000, Rate: 6%, Time: 1 year
 
+        // display current balance for each account
+        for (Account account : accounts) {
+            System.out.println("Current balance for " + account.getClass().getSimpleName() + ": "
+                    + String.format("%.2f", account.principal));
+        }
+
+        System.out.println("\n");
+
         // Calculate and display interest for each account
         for (Account account : accounts) {
             System.out.println("Interest for " + account.getClass().getSimpleName() + ": "
                     + String.format("%.2f", account.calculateInterest()));
         }
+
+        System.out.println("\n");
+
+        accounts.getFirst().withdraw(5000);
+
+        System.out.println("Current balance for " + accounts.getFirst().getClass().getSimpleName() + ": "
+                + String.format("%.2f", accounts.getFirst().principal));
+
+        accounts.getFirst().deposit(10000);
+
+        System.out.println("Current balance for " + accounts.getFirst().getClass().getSimpleName() + ": "
+                + String.format("%.2f", accounts.getFirst().principal));
     }
 }
